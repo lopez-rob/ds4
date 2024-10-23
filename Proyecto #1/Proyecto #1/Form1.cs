29a5b23ff22cf4f6213938980eaec5a2873df5c1
Roberto López <132418482+lopez-rob@users.noreply.github.com>
2024-10-22 20:51:54 -0500

    Add files via upload
    adelantos del proyecto, interfaz lista. solo háganla funcionar. :)

---
+++ Proyecto #1/Proyecto #1/Form1.cs
@@ -0,0 +1,105 @@
+namespace Proyecto__1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonDivision_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonRaiz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonElevado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonCE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonPor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonResta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonSuma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonIgual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonResultados_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
